e8d5813b699358923764cdc6b8c5c140c624403c
azizkh916 <azizkhouni9@gmail.com>
2025-06-01 04:50:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import nltk
-nltk.download("punkt")
-nltk.download("stopwords")
-
-from flask import Flask, request, render_template, send_file
 import os
-import io
+import re
+import nltk
+from flask import Flask, request, render_template, send_file
+from io import BytesIO
 import fitz  # PyMuPDF
 from fpdf import FPDF
 from sumy.parsers.plaintext import PlaintextParser
@@ -13,15 +11,20 @@ from sumy.summarizers.lsa import LsaSummarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 
+# Vérifier si punkt est déjà installé, sinon le télécharger
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
 app = Flask(__name__)
 
 LANGUAGE = "french"
 latest_summary_pdf = None
-import re
-
-def clean_text(text):
-    # Supprimer les caractères non supportés par Latin-1 (comme les emojis)
-    return re.sub(r'[^\x00-\xff]', '', text)
 
 def extract_text_from_pdf(pdf_file):
     text = ""
@@ -37,16 +40,17 @@ def summarize_text(text, sentence_count=5):
     summarizer.stop_words = get_stop_words(LANGUAGE)
     summary = summarizer(parser.document, sentence_count)
     return "\n".join(str(sentence) for sentence in summary)
+
 def create_pdf(summary_text):
     pdf = FPDF()
     pdf.add_page()
 
+    # Assurez-vous que le fichier DejaVuSans.ttf est bien présent dans le dossier de votre app
     pdf.add_font('DejaVu', '', 'DejaVuSans.ttf', uni=True)
     pdf.set_font('DejaVu', '', 14)
-    pdf.multi_cell(0, 10, clean_text(summary_text))  # nettoyer ici
+    pdf.multi_cell(0, 10, summary_text)
 
-    return BytesIO(pdf.output(dest='S').encode('latin1'))
-
+    return BytesIO(pdf.output(dest='S').encode('latin1', 'ignore'))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
